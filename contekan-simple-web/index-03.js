b98c2419d8a15ff6382c4112e2d37f844ddedaea
Asinkron Indonesia <asinkronindonesia@mymacs-MacBook-Pro.local>
2024-01-06 11:22:55 +0700

    optimize some codes

--- contekan-simple-web/index-03.js
+++ contekan-simple-web/index-03.js
@@ -2,26 +2,28 @@ const fs = require('fs'); // // panggil fungsi file
 const http = require('http');
 const portNumber = 5000;
 
-// definisikan template
-const homePageTemplate = fs.readFileSync('./templates/index.html');
-const aboutTemplate = fs.readFileSync('./templates/about.html');
-const contactTemplate = fs.readFileSync('./templates/contact.html');
-const notFoundTemplate = fs.readFileSync('./templates/notFound.html');
-
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
+        // definisikan template
+        const homePageTemplate = fs.readFileSync('./templates/index.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(homePageTemplate);
     }
 	else if(req.url === '/about') {
+        // definisikan template
+        const aboutTemplate = fs.readFileSync('./templates/about.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(aboutTemplate);
 	}
     else if(req.url === '/contact') {
+        // definisikan template
+        const contactTemplate = fs.readFileSync('./templates/contact.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(contactTemplate);
     }
 	else {
+        // definisikan template
+        const notFoundTemplate = fs.readFileSync('./templates/notFound.html');
         res.writeHead(404, {'content-type': 'text/html'});
         res.write(notFoundTemplate);
     }
--- contekan-simple-web/index-04.js
+++ contekan-simple-web/index-04.js
@@ -2,27 +2,25 @@ const fs = require('fs'); // // panggil fungsi file
 const http = require('http');
 const portNumber = 5000;
 
-// definisikan template
-const homePageTemplate = fs.readFileSync('./templates/index.html');
-const aboutTemplate = fs.readFileSync('./templates/about.html');
-const contactTemplate = fs.readFileSync('./templates/contact.html');
-const notFoundTemplate = fs.readFileSync('./templates/notFound.html');
-
-
 // definisikan file asset
 // const logoWebsite = fs.readFileSync('./assets/logo-website.png');
 
-
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
+        // definisikan template
+        const homePageTemplate = fs.readFileSync('./templates/index.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(homePageTemplate);
     }
 	else if(req.url === '/about') {
+        // definisikan template
+        const aboutTemplate = fs.readFileSync('./templates/about.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(aboutTemplate);
 	}
     else if(req.url === '/contact') {
+        // definisikan template
+        const contactTemplate = fs.readFileSync('./templates/contact.html');
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(contactTemplate);
     }
@@ -32,6 +30,8 @@ const server = http.createServer((req, res) => {
     //     res.end();
     // }
 	else {
+        // definisikan template
+        const notFoundTemplate = fs.readFileSync('./templates/notFound.html');
         res.writeHead(404, {'content-type': 'text/html'});
         res.write(notFoundTemplate);
     }
